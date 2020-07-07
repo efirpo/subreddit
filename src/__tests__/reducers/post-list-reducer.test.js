12dca7e9caa441d8db6e6b87efcd8108981b6674
Matt Stroud <lixingke06@gmail.com>
2020-07-07 11:53:23 -0700

    add ADD POST and DELETE POST reducer cases, tests for both

--- src/__tests__/reducers/post-list-reducer.test.js
+++ src/__tests__/reducers/post-list-reducer.test.js
@@ -1,7 +1,70 @@
 import postListReducer from '../../reducers/post-list-reducer';
 
 describe('postListReducer', () => {
+  let action;
+  const testPost = {
+    title: 'Test Post Number One',
+    id: 1,
+    image: "http://www.checkthisimageout.whoa/image1.png",
+    showPostDetail: false,
+    postDetail: {}
+  }
+
+  const testState = {
+    [1]: {
+      title: 'Test Post Number One',
+      id: 1,
+      image: "http://www.checkthisimageout.whoa/image1.png",
+      showPostDetail: false,
+      postDetail: {}
+    },
+    [2]: {
+      title: 'Zany Test Post Number Two',
+      id: 2,
+      image: "http://www.problem.bro/imagetroll.jpg",
+      showPostDetail: false,
+      postDetail: {}
+    }
+  }
+
   test('Should return default state ', () => {
     expect(postListReducer({}, { type: null })).toEqual({})
   })
+  test('Should add testPost to masterPostList', () => {
+    const { title, id, image, showPostDetail, postDetail } = testPost;
+    action = {
+      type: 'ADD_POST',
+      title: title,
+      id: id,
+      image: image,
+      showPostDetail: showPostDetail,
+      postDetail: postDetail
+    }
+    expect(postListReducer({}, action)).toEqual({
+      [id]: {
+        title: title,
+        id: id,
+        image: image,
+        showPostDetail: showPostDetail,
+        postDetail: postDetail
+      }
+    })
+  })
+  test('Should delete 2nd post from currentState Object', () => {
+    action = {
+      type: 'DELETE_POST',
+      id: 2
+    };
+    expect(postListReducer(testState, action)).toEqual({
+      1: {
+        title: 'Test Post Number One',
+        id: 1,
+        image: "http://www.checkthisimageout.whoa/image1.png",
+        showPostDetail: false,
+        postDetail: {}
+      }
+    })
+  })
+
+
 })
--- src/reducers/post-list-reducer.js
+++ src/reducers/post-list-reducer.js
@@ -1,3 +1,22 @@
 export default (state = {}, action) => {
-  return state
+
+  const { title, id, image, showPostDetail, postDetail } = action;
+  switch (action.type) {
+    case 'ADD_POST':
+      return Object.assign({}, state, {
+        [id]: {
+          title: title,
+          id: id,
+          image: image,
+          showPostDetail: showPostDetail,
+          postDetail: postDetail
+        }
+      });
+    case 'DELETE_POST':
+      const newState = { ...state };
+      delete newState[id];
+      return newState;
+    default:
+      return state;
+  }
 }
